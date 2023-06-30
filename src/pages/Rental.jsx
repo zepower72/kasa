0e2f651e58b898b6b9468b6be4067d6e04c6e3cd
zepower72 <114823832+zepower72@users.noreply.github.com>
2023-06-30 21:08:28 +0200

    again

--- src/pages/Rental.jsx
+++ src/pages/Rental.jsx
@@ -34,54 +34,55 @@ export default function Rental() {
     const arrayHost = picked.host.name.split(" ");
     firstName = arrayHost.shift();
     lastName = arrayHost.join(" ");
-    /*La méthode split(" ")découpe la chaîne de caractères nameen un tableau de sous-chaînes, en utilisant l'espace comme séparateur. Par exemple, si rent.host.nameest "John Doe", alors arrayHostsera un tableau contenant ["John", "Doe"].
+  }
+  /*La méthode split(" ")découpe la chaîne de caractères nameen un tableau de sous-chaînes, en utilisant l'espace comme séparateur. Par exemple, si rent.host.nameest "John Doe", alors arrayHostsera un tableau contenant ["John", "Doe"].
 Ensuite, la ligne firstName = arrayHost.shift()utilise la méthode shift()sur le tableau arrayHostpour extraire et supprimer le premier élément du tableau, et le stocker dans la variable firstName.
 La méthode shift()retourne l'élément supprimé, donc firstNamecontiendra la première partie du nom de l'hôte (dans l'exemple précédent, firstNamesera "John").
 Enfin, la ligne lastName = arrayHost.join(" ")utilise la méthode join(" ")sur le tableau arrayHostpour concaténer tous les éléments restants du tableau en une seule chaîne de caractères, séparés par un espace. Le résultat est stocké dans la variable lastName.
 Ainsi, si rentest défini et non faux, le code extraira le prénom et le nom de l'hôte à partir de la propriété namede l'objet rent.hostet les stockera dans les variables firstNameet lastNamerespectivement.*/
-    return (
-      <div className="rental__container">
-        <Main>
-          <div>
-            <Carrousel pictures={picked.pictures} />
+  const equipments = picked.equipments;
+  const EquipmentsList = equipments.map((item, index) => (
+    <li key={index} className="EquipmentsList">
+      {item}
+    </li>
+  ));
+
+  return (
+    <div className="rental__container">
+      <Main>
+        <div>
+          <Carrousel pictures={picked.pictures} />
+        </div>
+
+        <section className="rental__presentation">
+          <div className="rental__title">
+            <h1>{picked.title}</h1>
+            <p className="rental__location">{picked.location}</p>
           </div>
 
-          <section className="rental__presentation">
-            <div className="rental__title">
-              <h1>{picked.title}</h1>
-              <p className="rental__location">{picked.location}</p>
-            </div>
+          <div className="rental__host">
+            <HostName firstName={firstName} lastName={lastName} />
+            <Host name={picked.host.name} profil={picked.host.picture} />
+          </div>
+        </section>
 
-            <div className="rental__host">
-              <HostName firstName={firstName} lastName={lastName} />
-              <Host name={picked.host.name} profil={picked.host.picture} />
-            </div>
-          </section>
+        <div className="rental__info">
+          <Tags tags={picked.tags} />
+          <Rating rating={picked.rating} />
+        </div>
 
-          <div className="rental__info">
-            <Tags tags={picked.tags} />
-            <Rating rating={picked.rating} />
+        <section className="dropdown__container">
+          <div className="DD__title">
+
+            <Dropdown body={picked.description} />
           </div>
 
-          <section className="dropdown__container">
-            <Dropdown title="Description">
-              <p className="dropdown__body">{picked.description}</p>
-            </Dropdown>
-            <Dropdown title="Équipements">
-              <ul className="dropdown__body">
-                {picked.equipments.map((item, index) => (
-                  <li key={index} className="dropdown__body__item">
-                    {item}
-                  </li>
-                ))}
-              </ul>
-            </Dropdown>
-          </section>
-        </Main>
-      </div>
-      
-    )
-                
-
-  }
+          <div className="DD__title">
+            <Dropdown body={EquipmentsList} />
+          </div>
+        </section>
+        
+      </Main>
+    </div>
+  );
 }
