cf51ab9892e152dd5ece71ff5db90b87f36374a4
zepower72 <114823832+zepower72@users.noreply.github.com>
2023-07-13 17:31:04 +0200

    Update Carousel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -17,26 +17,34 @@ function Carrousel({ pictures }) {
     setCurrentSlide((currentSlide - 1 + totalSlides) % totalSlides);
   };
 
-  return (
-    <div className="Carrousel__Container">
-      <img
-        className="Carrousel__btnLeft"
-        src={LeftChevron}
-        alt=""
-        onClick={prevSlide}
-      />
-      <img
-        className="Carrousel__btnRight"
-        src={RightChevron}
-        alt=""
-        onClick={nextSlide}
-      />
-      <div className="Carrousel__counter">
-        {currentSlide + 1}/{totalSlides}
+  if (pictures.length <= 1) {
+    // Si le nombre d'images est inférieur ou égal à 1, n'affichez pas les boutons Suivant et Précédent
+    return (
+      <div className="Carrousel__Container">
+        <img className="Carrousel__image" src={pictures[currentSlide]} alt="" />
       </div>
-      <img className="Carrousel__image" src={pictures[currentSlide]} alt="" />
-    </div>
-  );
+    );
+  } else
+    return (
+      <div className="Carrousel__Container">
+        <img
+          className="Carrousel__btnLeft"
+          src={LeftChevron}
+          alt=""
+          onClick={prevSlide}
+        />
+        <img
+          className="Carrousel__btnRight"
+          src={RightChevron}
+          alt=""
+          onClick={nextSlide}
+        />
+        <div className="Carrousel__counter">
+          {currentSlide + 1}/{totalSlides}
+        </div>
+        <img className="Carrousel__image" src={pictures[currentSlide]} alt="" />
+      </div>
+    );
 }
 
 export default Carrousel;
